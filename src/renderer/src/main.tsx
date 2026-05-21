bcf594a92eb65b515d76af441a498321489c834b
Roman Voitiuk <roman.voitiuk@student.tuke.sk>
2026-05-21 22:26:43 +0300

    Fix print styles and fix table layout in web PDF

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { isElectron } from '@renderer/utils/environment';
+
+if (!isElectron()) {
+  document.body.classList.add('web-version');
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
